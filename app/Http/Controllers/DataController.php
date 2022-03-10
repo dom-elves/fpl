d5300544625cbaf5f83ce4a3491d50fe4cfe8173
dom-elves <dominic.elves@netmatters-scs.com>
2022-03-10 10:22:27 +0000

    created player model and updated & tested db query

--- app/Http/Controllers/DataController.php
+++ app/Http/Controllers/DataController.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use App\Http\Controllers\APIController;
 use App\Providers\AveragesProvider;
+use App\Models\Player;
 
 class DataController extends Controller
 {
@@ -26,7 +27,7 @@ class DataController extends Controller
     //for some reason, object returns in an array - making it always pos 0 takes it out of array 
     public function returnSelectedPlayer($id)
     {
-        $collection = DB::table('players')->where('id', '=', $id)->get();
+        $collection = Player::where('id', '=', $id)->get();
         
         $player = $collection[0]; //figure out how to take the object out of the array, casting to object doesn't work somehow?
         
---
+++ app/Models/Player.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Player extends Model
+{
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'players';
+
+    /**
+     * The primary key associated with the table.
+     *
+     * @var string
+     */
+    protected $primaryKey = 'id';
+}
