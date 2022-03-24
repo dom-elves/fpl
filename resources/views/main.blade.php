2279365c81a6198c5f44d10b7db186baf4197f0e
dom-elves <dominic.elves@netmatters-scs.com>
2022-03-24 11:50:58 +0000

    some style changes to keep things thematic

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -25,11 +25,11 @@
 
       @foreach ($teams as $team)
       
-      <div class="border-2 border-[#00ff85] p-1 m-2">
-      <p class="text-white">{{ $team->team_name }}</p>
+      <div class="border-2 border-[#00ff85] p-1 m-2 bg-purple-50 w-[270px]">
+      <p class="text-center font-bold">{{ $team->team_name }}</p>
 
         @foreach ($players as $player)
-          <a href="player/{{ $player->player_id }}"><p class="text-white">{{ App\Http\Controllers\TeamsController::checkTeam($player, $team) }}</p></a>
+          <a href="player/{{ $player->player_id }}"><p class="text-center">{{ App\Http\Controllers\TeamsController::checkTeam($player, $team) }}</p></a>
         @endforeach
 
     
